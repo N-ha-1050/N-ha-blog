db8453a393211a6c4bc7e65999a2eb25c4f893ff
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2025-02-28 18:23:41 +0900

    ➕ prettier を導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
-  title: {
-    template: "%s | N_ha's Blog",
-    default: "N_ha's Blog",
-  },
-  description: "N_ha のブログ(個人的なメモ)",
-  icons: "/favicon.svg",
-};
+    title: {
+        template: "%s | N_ha's Blog",
+        default: "N_ha's Blog",
+    },
+    description: "N_ha のブログ(個人的なメモ)",
+    icons: "/favicon.svg",
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="ja">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="ja">
+            <body>{children}</body>
+        </html>
+    )
 }
