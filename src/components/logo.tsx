47efd24011ca02e8780470130c858bb9f90a63f4
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2025-03-01 00:50:27 +0900

    トップページを作成

---
+++ src/components/logo.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import { buttonVariants } from "./ui/button"
+
+export function Logo() {
+    return (
+        <Link
+            href="/"
+            className={buttonVariants({
+                variant: "link",
+                className: "my-4 p-4 text-5xl",
+            })}
+        >
+            N_ha
+        </Link>
+    )
+}
