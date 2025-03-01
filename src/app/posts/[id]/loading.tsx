9a1729afa89eb6ca4214efb243b0237c02aa799a
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2025-03-02 01:32:58 +0900

    ✨ 記事ページのloading.tsxを作成

--- src/app/posts/[id]/loading.tsx
+++ src/app/posts/[id]/loading.tsx
@@ -1,3 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
 export default function Loading() {
-    return <div>Loading...</div>
+    return (
+        <div>
+            <Skeleton className="mb-4 h-16 w-full" />
+            <Skeleton className="mb-8 h-8 w-64" />
+            <Skeleton className="mb-4 h-8 w-full" />
+            <Skeleton className="mb-4 h-8 w-full" />
+            <Skeleton className="mb-4 h-8 w-full" />
+            <Skeleton className="mb-4 h-8 w-full" />
+        </div>
+    )
 }
