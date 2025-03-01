dd10521a4d79cf5080027dd4c5530a82704d1fdf
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2025-03-01 20:30:37 +0900

    💄 Markdown に対応して記事ページのUIを追加

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,6 +1,14 @@
+import { markReact } from "@/lib/markdown"
 import { prisma } from "@/lib/prisma"
 import { notFound } from "next/navigation"
+import { TagList } from "@/components/tag/tag-list"
 
+const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+}
 export default async function Post(props: { params: Promise<{ id: string }> }) {
     const { id } = await props.params
     if (!id || id.length !== 36) notFound()
@@ -9,10 +17,18 @@ export default async function Post(props: { params: Promise<{ id: string }> }) {
         include: { tags: true },
     })
     if (!post) notFound()
+    const createdAt = new Date(post.createdAt)
+    const updatedAt = new Date(post.updatedAt)
+    const content = await markReact(post.content)
     return (
         <div>
-            <h1>{post.title}</h1>
-            <p>{post.content}</p>
+            <h1 className="mb-4 text-4xl font-bold">{post.title}</h1>
+            <TagList className="mb-4 text-lg" tags={post.tags} isLink />
+            <div className="mb-8 md:flex md:gap-8">
+                <p>作成日 {createdAt.toLocaleDateString(undefined, options)}</p>
+                <p>更新日 {updatedAt.toLocaleDateString(undefined, options)}</p>
+            </div>
+            <div className="prose prose-lg max-w-none">{content}</div>
         </div>
     )
 }
