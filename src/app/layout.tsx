05a9fe6c9f20bd07b566fa2895635f7333312d66
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2025-03-13 14:10:57 +0900

    💄 アイコンを微調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import "./globals.css"
+import "@/app/globals.css"
 import { Header } from "@/components/layout/header"
 import { ThemeProvider } from "@/components/theme/theme-provider"
 
--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
     return (
         <Link
             href="/"
-            className="my-4 rounded-full border-2 border-transparent p-4 text-4xl text-foreground duration-200 hover:border-foreground hover:opacity-80"
+            className="my-2 rounded-full border-2 border-transparent p-4 text-4xl text-foreground duration-200 hover:border-foreground"
         >
             N_ha
         </Link>
