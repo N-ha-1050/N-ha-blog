878cbf1da13414e216f7f43d216a8757b3788ddd
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2025-03-14 04:46:16 +0900

    💄 フォントを追加

---
+++ src/lib/fonts.ts
@@ -0,0 +1,4 @@
+import { Noto_Sans_JP, Noto_Sans_Mono } from "next/font/google"
+
+export const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] })
+export const notoSansMono = Noto_Sans_Mono({ subsets: ["latin"] })
