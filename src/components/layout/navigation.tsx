72ad92a946a49f6ff1aeb049f6f82e52ec2137b7
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2025-03-14 04:13:25 +0900

    🐛 ナビゲーションバーでログインページに移動した際にバーが閉じないバグを修正

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -35,6 +35,7 @@ export function Navigation({
                             variant: "ghost",
                             size: "icon",
                         })}
+                        onClick={() => setOpen(!isOpen)}
                     >
                         <User />
                     </Link>
